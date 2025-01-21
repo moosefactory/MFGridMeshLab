4e1c8e0e98f23515213fce76699ea7e506d5da02
Tristan Leblanc <tristan@moosefactory.eu>
2025-01-21 23:04:34 +0100

    Builds with latest MFSCNExtensions ( 1.0.5 ) More readme

--- MFGridMeshLab Shared/GameController.swift
+++ MFGridMeshLab Shared/GameController.swift
@@ -6,6 +6,7 @@
 //
 
 import SceneKit
+import MFFoundation
 import MFSCNExtensions
 import MFGridUtils
 
@@ -37,16 +38,51 @@ class GameController: NSObject, SCNSceneRendererDelegate {
         // Add a mesh node ( must import MFSCNExtension and MFGridUtils )
         
         // 1 - Create a grid object
-        let grid = MFGrid(gridSize: 40, cellSize: 2)
         
-        // 2 - Create a mesh using the grid
-        let meshNode = MFSCNGridMeshNode(grid: grid)
+        let grid = MFGrid(gridSize: 150, cellSize: 0.8)
         
-        // 3 - Slightly move the mesh down so wee see it in perspective
-        meshNode.position.y = -3.0
+        // 2 - There we define a height function.
+        //
+        // It basically makes a linear clif with a bit of variation
+        // So in one case a height with a bit of random is returned,
+        // in the other case, we are at sea level, with a bit of random two to give a bit of waves effect
+        
+        let heightBlock: MFSCNHeightComputeBlock = { _, loc, locp in
+            if (loc.h < Int(Double.random(3)) + loc.v / 2) {
+                return 4.0 + Double.random(1.2)
+            }
+            return Double.random(0.25)
+        }
+        
+        // 3 - We define a color compute function to set a color per vertice
+        //
+        // We choose blue if the level is 0, else we use a mixed red/green color
 
-        scene.rootNode.addChildNode(meshNode)
+        let colorBlock: MFSCNColorComputeBlock = { value, gridLoc, location, vertice in
+                        
+            if vertice.z > 0.2 {
+                return SCNVector4(x: vertice.z / 7, y: vertice.z / 3, z: 0, w: 1)
+            } else {
+                return SCNVector4(x: 0, y: 0, z: 1, w: 1)
+            }
+        }
         
+        // 4 - Create a mesh using the grid
+
+        let meshNode = MFSCNGridMeshNode(grid: grid,
+                                         heightComputeBlock: heightBlock,
+                                         colorComputeBlock: colorBlock)
+        
+        // 5 - That's it - Slightly move the mesh down so wee see it in perspective
+        
+        meshNode.position.y = -3.0
+        if let material = meshNode.geometry?.materials.first {
+            material.diffuse.contents = SCNMaterialProperty(contents: SCNColor.orange)
+            material.isDoubleSided = true
+            material.isLitPerPixel = true
+        }
+        
+        scene.rootNode.addChildNode(meshNode)
         sceneRenderer.scene = scene
     }
     
@@ -61,17 +97,13 @@ class GameController: NSObject, SCNSceneRendererDelegate {
             // highlight it
             SCNTransaction.begin()
             SCNTransaction.animationDuration = 0.5
-            
             // on completion - unhighlight
             SCNTransaction.completionBlock = {
                 SCNTransaction.begin()
                 SCNTransaction.animationDuration = 0.5
-                
                 material.emission.contents = SCNColor.black
-                
                 SCNTransaction.commit()
             }
-            
             material.emission.contents = SCNColor.red
             
             SCNTransaction.commit()
--- README.md
+++ README.md
@@ -6,7 +6,13 @@ A demo app that demonstrates the **MFGridMeshNode** object, based on the Apple *
 
 <https://github.com/moosefactory/MFSCNExtensions>
 
+## Discussion
+
 The code is slightly modified to insert a mesh under the rotating plane.
+
+### 1 - Create a mesh object
+
+![MeshLabScreenshot Image](MeshLabScreenshot.jpg)
 
 ```
 
@@ -21,13 +27,54 @@ let meshNode = MFSCNGridMeshNode(grid: grid)
 // 3 - Slightly move the mesh down so wee see it in perspective
 meshNode.position.y = -3.0
 
+scene.rootNode.addChildNode(meshNode)
 sceneRenderer.scene = scene
 
 ```
 
 Build and run.
 
-![MeshLabScreenshot Image](MeshLabScreenshot.jpg)
+### 2 - Add some depth and color
+
+#### Define a height computation function.
+
+```
+// It basically makes a linear clif with a bit of variation
+// So in one case a height of 4 with a bit of random is returned,
+// in the other case, we are at sea level, with a bit of random two to give a bit of waves effect
+
+let heightBlock: MFSCNHeightComputeBlock = { _, loc, locp in
+    if (loc.h < Int(Double.random(3)) + loc.v / 2) {
+        return 4.0 + Double.random(1.2)
+    }
+    return Double.random(0.25)
+}
+```
+
+#### Define a color computation function
+
+![MeshLabScreenshot Image](MeshLabScreenshot2.jpg)
+
+```
+// We choose blue if the level is 0, else we use a mixed red/green color
+
+let colorBlock: MFSCNColorComputeBlock = { value, gridLoc, location, vertice in
+
+    if vertice.z > 0.2 {
+        return SCNVector4(x: vertice.z / 7, y: vertice.z / 3, z: 0, w: 1)
+    } else {
+        return SCNVector4(x: 0, y: 0, z: 1, w: 1)
+    }
+}
+
+### We create the  mesh
+// 4 - Create a mesh using the grid
+
+let meshNode = MFSCNGridMeshNode(grid: grid,
+                                 heightComputeBlock: heightBlock,
+                                 colorComputeBlock: colorBlock)
+
+```
 
 --
 
